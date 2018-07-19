d31082f6946166bb6dc0d59fd04e662923dfb53f
Fraser Darwent <fraser@darwen.tf>
2018-07-19 14:29:45 +0100

    Work in progress developing player

--- src/js/fullscreenplayer.js
+++ src/js/fullscreenplayer.js
@@ -5,15 +5,28 @@ const fullscreenplayer = {
   }
 }
 
-function fullscreenplayerTable(args){
+function fullscreenplayerTable(args) {
   switch (args.state) {
-  case 'start':{
+  case 'start': {
     args.node.textContent = 'Loading'
     break
   }
-  case 'finish':{
-      console.log(args)
-      render('fullscreenplayerTable', args.result, args.node)
+  case 'finish': {
+    console.log(args)
+    args.result.results.forEach((result) => {
+      result.duration = Math.floor(result.duration)
+      result.bpm = Math.floor(result.bpm)
+      result.artist = ''
+      result.artists.forEach((artist, index) => {
+        result.artist += `${artist.name}${(index + 1 < result.artists.length) ? ', ' : ''}`
+      })
+    })
+    render('fullscreenplayerTable', args.result, args.node)
   }
   }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  findNode('header').classList.toggle('hidden', window.location.pathname.match(/^\/player/))
+  findNode('footer').classList.toggle('hidden', window.location.pathname.match(/^\/player/))
+})
