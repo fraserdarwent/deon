e8a6770fe8fab6e5d014b2c4b57f04bff1f7a798
Fraser Darwent <fraser@darwen.tf>
2018-07-20 16:20:22 +0100

    Changes

--- src/js/fullscreenplayer.js
+++ src/js/fullscreenplayer.js
@@ -2,13 +2,19 @@ const fullscreenplayer = {
   selector: '#fullscreenplayer',
   tables: {
     selector: '.table'
+  },
+  loadMore: function () {
+    const table = this.closest('tbody')
+
+    table.setAttribute('data-source', `$endpoint/catalog/browse?skip=${table.childElementCount}`)
+    loadNodeSources(this.closest('table'))
   }
 }
 
 function fullscreenplayerTable(args) {
   switch (args.state) {
   case 'start': {
-    args.node.textContent = 'Loading'
+    // args.node.textContent = 'Loading'
     break
   }
   case 'finish': {
@@ -20,8 +26,11 @@ function fullscreenplayerTable(args) {
       result.artists.forEach((artist, index) => {
         result.artist += `${artist.name}${(index + 1 < result.artists.length) ? ', ' : ''}`
       })
+
+      const div = render('fullscreenplayerTableRow', result, document.createElement('tbody'))
+
+      args.node.appendChild(div.firstElementChild)
     })
-    render('fullscreenplayerTable', args.result, args.node)
   }
   }
 }
